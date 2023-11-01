a56c2dc27ac9094e58e1a929972ac695203a0948
MOH-Shafizadegan <mh.shafizadegan@gmail.com>
2023-11-01 22:13:50 +0330

    Test file updated.

--- Code/signal_processing.py
+++ Code/signal_processing.py
@@ -28,15 +28,16 @@ def bp_filter(data, f_low, f_high, order, fs):
 
     return filtered_data
 
-def remove_ICA(data, Th):
+def remove_ICA(data):
     # Run ICA on your data
+    data = data.T
     ica = FastICA(n_components=data.shape[1]) # we want *all* the components
     ica.fit(data)
 
     # Decompose your data into independent components
     components = ica.transform(data) # shape = (n_samples, n_components)
 
-    remove_indices = list(np.where(np.max(components, 0) > Th))
+    remove_indices = list(np.where(np.max(components, 0) > 0.75*np.max(abs(components))))
 
     # "remove" unwanted components by setting them to 0 - simplistic but gets the job done
     components[:, remove_indices] = 0
